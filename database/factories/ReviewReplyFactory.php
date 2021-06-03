7ee425ed1455a7e00275eea6ff5fb6f0b412bdeb
Hemant Durdhawale <hemantd983@gmail.com>
2021-06-03 19:21:23 +0530

    update the reviewFactory

--- database/factories/ReviewReplyFactory.php
+++ database/factories/ReviewReplyFactory.php
@@ -7,6 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(ReviewReply::class, function (Faker $faker) {
     return [
-        'reply' => $faker->text(20),
+        'reply' => $faker->sentence,
     ];
 });
