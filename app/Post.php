040180bf03f8c2d4ae467e58ab58da1c72c2d659
Hemant Durdhawale <hemantd983@gmail.com>
2021-06-04 10:24:25 +0530

    added functions to get posts with most number os review replies

--- app/Post.php
+++ app/Post.php
@@ -16,4 +16,44 @@ class Post extends Model implements ReviewRateable
 	{
 		return $this->belongsTo(User::class);
 	}
+
+	public function getPostsWithReviewsGt($limit) {
+		$result = [];
+		$posts = Post::all();
+
+
+		foreach($posts as $p) {
+			$count = $p->getAllRatings($p)->count();
+			if($count >= $limit ) {
+				$a = [
+					'post_id' => $p->id,
+					'count of reivews' => $count,
+				];
+
+				array_push($result, $a);
+			}
+		}
+
+		return $result;
+	}
+
+	public function getPostsWithReviewsLt($limit) {
+		$result = [];
+		$posts = Post::all();
+
+
+		foreach($posts as $p) {
+			$count = $p->getAllRatings($p)->count();
+			if($count <= $limit ) {
+				$a = [
+					'post_id' => $p->id,
+					'count of reivews' => $count,
+				];
+
+				array_push($result, $a);
+			}
+		}
+
+		return $result;
+	}
 }
